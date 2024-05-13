73a8be52bb35655359f101aa8d2295bbcf716aac
Rishabh Singh <rsingh@edvak.com>
2024-05-14 02:01:37 +0530

    ES-6 modules import export

---
+++ Practice-ES6-modules/main.mjs
@@ -0,0 +1,5 @@
+import {add, mul, sub} from './mathfn.mjs'
+
+console.log(add(3,4));
+console.log(mul(3,4));
+console.log(sub(4,3));
--- Practice-module/prac.mjs
+++
@@ -1,5 +0,0 @@
-import { add, sub, mul } from './maath.mjs';
-
-console.log(add(2,3));
-console.log(sub(2,3));
-console.log(mul(2,3));
